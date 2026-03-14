c5bc9d5ed0ee7e80ecb0857a2ea26320deb4cae3
Katie Kloss <katherine.kloss@proton.me>
2026-03-14 05:12:52 -0400

    the hello world of PKCS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use cryptoki::context::{CInitializeArgs, CInitializeFlags};
+
 fn main() {
-    println!("Hello, world!");
+    let ctx = cryptoki::context::Pkcs11::new("/usr/local/lib/libykcs11.dylib").expect("Cannot load PKCS impl");
+    ctx.initialize(CInitializeArgs::new(CInitializeFlags::OS_LOCKING_OK)).expect("Cannot initialize PKCS");
+
+    let slots = ctx.get_slots_with_token().expect("Can't get slots");
+    if slots.len() == 0 {
+        panic!("No slots found");
+    }
+
+    let slot = slots[0];
+    println!("{:?}", slot);
+    let info = ctx.get_slot_info(slot).expect("Can't get slot info");
+    println!("{:?}", info);
 }
